b49ec394ecd8399f7ebca2a43b7bee4b197866c0
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-04-08 14:03:31 +0530

    Connect with Mongoose DB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const app= require('./app')
 const dotenv = require('dotenv')
+const connectDatabase = require('./config/database')
 
 //Config
 dotenv.config({path:"backend/config/config.env"})
 
+//connect database
+connectDatabase()
 app.listen(process.env.PORT,()=>{
     console.log(`Server is Working on http://localhost:${process.env.PORT}`)
 })
