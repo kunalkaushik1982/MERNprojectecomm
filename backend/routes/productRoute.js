9ba1bc2efcc1230e87981be76fc869625801449b
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-04-13 20:25:49 +0530

    Implemented Logout Route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,9 +5,9 @@ const {isAuthenticatedUser} = require("../middleware/auth")
 const router=express.Router()
 
 router.route('/getallproducts').get(isAuthenticatedUser,getAllProducts)
-router.route('/createproduct/new').post(createProduct)
-router.route('/updateproduct/:id').put(updateProduct)
-router.route('/deleteproduct/:id').delete(deleteProduct)
+router.route('/createproduct/new').post(isAuthenticatedUser,createProduct)
+router.route('/updateproduct/:id').put(isAuthenticatedUser,updateProduct)
+router.route('/deleteproduct/:id').delete(isAuthenticatedUser,deleteProduct)
 router.route('/getproduct/:id').get(getProductDetails)
 
 module.exports = router
