5c19215059d6c9fc3c68a7d103b280df8e1b283e
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-04-14 00:21:21 +0530

    Implemented Reset Password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
 const sendEmail = require("../utils/sendEmail")
+const crypto = require("crypto")
 
 //Register a User
 
@@ -58,4 +59,20 @@ exports.forgetPassword = catchAsyncErrors(async (req,res,next)=>{
     await user.save({validateBeforeSave:false})
     return next(new ErrorHander(error.message,404))
   }
+})
+
+//Reset Password
+
+exports.resetPassword = catchAsyncErrors(async (req,res,next)=>{
+
+  //creating token hash
+    const resetPasswordToken = crypto.createHash("sha256").update(req.params.token).digest("hex")
+    const user = await User.findOne({resetPasswordToken,resetPasswordExpire:{$gt:Date.now()}})
+    if(!user){return next(new ErrorHander("Reset Password Token is invalid or has been expired",400))}
+    if(req.body.password !== req.body.confirmPassword){return next(new ErrorHander("Password doesn't match",400))}
+    user.password=req.body.password
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+    await user.save()
+    sendToken(user,200,res)
 })
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router=express.Router()
-const { registerUser, loginUser,logoutUser, forgetPassword } = require("../controllers/userController")
+const { registerUser, loginUser,logoutUser, forgetPassword,resetPassword } = require("../controllers/userController")
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/password/forget').post(forgetPassword)
+router.route('/password/reset/:token').put(resetPassword)
 router.route('/logout').get(logoutUser)
 
 module.exports = router
